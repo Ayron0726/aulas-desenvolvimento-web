5bd8e9767549b98ac8fdac2be90042723587b7b3
Ayron0726 <ayron0726@hotmail.com>
2022-10-31 20:26:37 -0300

    1

---
+++ teste.php
@@ -0,0 +1,14 @@
+$opcao = "";
+
+switch($opcao){
+
+    case's':
+        echo "Você escolheu sim!";
+        break;
+    case'n':
+        echo"Você escolheu não!";
+        break;
+    default:
+        echo"A opção é inválida";
+        break;
+}
